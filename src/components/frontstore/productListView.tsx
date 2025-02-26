015cc6ac23c36adf8ddf4e8ccb14e81fe8ed74ba
azizktata <65952489+azizktata@users.noreply.github.com>
2025-02-26 09:28:08 +0100

    frontstore v2

--- src/components/frontstore/productListView.tsx
+++ src/components/frontstore/productListView.tsx
@@ -8,6 +8,7 @@ import { addToCart } from "@/store/slices/cartSlice";
 import { useAppDispatch } from "@/store/hooks";
 import { ShoppingBasket } from "lucide-react";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function ProductListView({
   products,
@@ -25,7 +26,7 @@ export default function ProductListView({
               width={312}
               height={269}
               alt={"fallback image"}
-              className="w-[312px] h-[280px] md:h-[269px] w-full object-contain  rounded-md 
+              className="w-[312px] h-[200px] sm:h-[280px] md:h-[269px] w-full object-contain  rounded-md 
                transition-transform duration-300 ease-in-out group-hover:scale-110"
             />
             <Badge className="absolute top-8 right-0 border-white text-storeAccent hover:bg-storeAccent hover:text-white bg-white text-xs font-medium py-1 px-2 rounded-l-full">
@@ -46,9 +47,13 @@ export default function ProductListView({
             )}
           </div>
 
-          <div className="flex flex-col items-start">
+          <div className="flex flex-col items-start  ">
             <div className="flex flex-col items-start  w-full ">
-              <p className="text-lg sm:text-2xl font-medium">{product.name}</p>
+              <Link href={`/products/${product.slug}`}>
+                <p className="text-base sm:text-lg md:text-2xl font-medium">
+                  {product.name}
+                </p>
+              </Link>
 
               <div className="flex flex-wrap justify-center items-center gap-x-2 mt-2 ">
                 {product.prices?.discount !== 0 && product.prices?.discount ? (
@@ -69,28 +74,30 @@ export default function ProductListView({
                   </span>
                 )}
               </div>
-              <div className="border-b border-gray-300 py-4 w-1/2">
+              <div className=" py-4 w-full">
                 {product.category && (
-                  <Badge className="text-xs lowercase font-normal text-storeSecondary bg-white rounded-full py-1 px-3 border border-blue-300 hover:bg-blue-200 transition-all duration-200 ease-in-out shadow-sm hover:shadow-md">
+                  <Badge className="text-[8px] sm:text-xs  lowercase font-normal text-storeSecondary bg-white rounded-full py-1 px-3 border border-blue-300 hover:bg-blue-200 transition-colors duration-200 ease-in-out shadow-sm hover:shadow-md">
                     {product.category[product.category.length - 1].name}
                   </Badge>
                 )}
               </div>
-              <p className="text-sm text-gray-500 dark:text-darkmode-dark mt-6 mb-8">
-                {product.description}
-              </p>
-              <div className="flex gap-4">
-                <a
-                  onClick={() =>
-                    dispatch(addToCart({ item: product, quantity: 1 }))
-                  }
-                  href="#sidebar"
-                >
-                  <Button className="flex items-center px-7 py-4 rounded-md bg-storeSecondary self-center text-white font-base transition duration-200 hover:bg-white hover:text-storeSecondary border-2 border-transparent hover:border-storeSecondary">
-                    <ShoppingBasket className="size-8 " /> Add to cart
-                  </Button>
-                </a>
+              <div className="hidden sm:flex border-t border-gray-300">
+                <p className="  text-sm text-gray-500 dark:text-darkmode-dark mt-6 mb-8">
+                  {product.description}
+                </p>
               </div>
+            </div>
+            <div className="flex gap-4   mt-auto">
+              <a
+                onClick={() =>
+                  dispatch(addToCart({ item: product, quantity: 1 }))
+                }
+                href="#sidebar"
+              >
+                <Button className="flex items-center px-7 py-4 rounded-md bg-storeSecondary self-center text-white font-base transition duration-200 hover:bg-white hover:text-storeSecondary border-2 border-transparent hover:border-storeSecondary">
+                  <ShoppingBasket className="size-8 " /> Add to cart
+                </Button>
+              </a>
             </div>
           </div>
         </div>
